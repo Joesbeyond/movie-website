980239e785ff72edebbc5d85ab3ce4f7505347c2
joesbeyond <joesbeyond@gmail.com>
2015-04-14 18:24:44 +0800

    add pages and reset directory

--- app.js
+++ app.js
@@ -6,4 +6,32 @@ app.set('views', './views');
 app.set('view engine', 'jade');
 app.listen(port);
 
-console.log('started on port' + port);
+console.log('started on port ' + port);
+
+//index page
+app.get('/', function(req, res) {
+	res.render('index', {
+	    title: 'Home'
+	})
+})
+
+//detail page 
+app.get('/movie/:id', function(req, res) {
+	res.render('detail', {
+	    title: 'detail'
+	})
+})
+
+//admin page
+app.get('/admin/movie', function(req, res) {
+	res.render('admin', {
+	    title: 'admin'
+	})
+})
+
+//list page
+app.get('/admin/list', function(req, res) {
+	res.render('list', {
+	    title: 'list'
+	})
+})
